923f3e696d7b1eac8d8010b02cca80ad20df3c98
jucsilva1 <82848094+jucsilva1@users.noreply.github.com>
2025-03-04 18:29:31 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
   login.style.display = "none";
   chat.style.display = "flex";
 
-  websocket = new WebSocket("wss://chat-backend-5dfh.onrender.com");
+  websocket = new WebSocket("wss://chat-backend-jcs.onrender.com");
   websocket.onmessage = processMessage;
 };
 
